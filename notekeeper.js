0ba16d9329854e8fba2712f07cde5e35f34ccd6c
Evgenii Klimov <disturh@gmail.com>
2018-01-20 23:48:03 +0300

    interface remake

--- notekeeper.js
+++ notekeeper.js
@@ -10,7 +10,8 @@ global.storage = {};
 mainFLOW.steps = {
     'start': getConfig,
     'config is ready': getDatabase,
-    'DB is ready': createInterface,
+    'DB is ready': parseTagsFromBase,
+    'tags are parsed': createInterface,
     'finish': closeApp,
 };
 mainFLOW.to('start');
@@ -28,15 +29,48 @@ function getDatabase() {
     console.log(`trying to read: ${storage.config.dbPath}`);
     FILE.readOrMake(
         storage.config.dbPath,
-        (content) => { storage.db = content; mainFLOW.to('finish') }
+        (content) => { storage.db = content; mainFLOW.to('DB is ready') }
     );
 }
-function createInterface() {
-    console.log(`trying to read: ${storage.config.dbPath}`);
-    FILE.readOrMake(
-        storage.config.dbPath,
-        (content) => { storage.db = content; mainFLOW.to('finish') }
-    );
+function parseTagsFromBase() {
+    let tags = storage.db.match(/\b___\w+/g);
+    if (tags) {
+        tags = removeClonesFrom(tags);
+    } else {
+        tags = [];
+    }
+
+    mainFLOW.to('tags are parsed', tags);
+}
+function removeClonesFrom(arr) {
+    for (var i = arr.length; i--;) {
+        for (var j = i; j--;) {
+            if (arr[j] === arr[i]) {
+                arr.splice(i, 1);
+                break;
+            }
+        }
+    }
+    return arr;
+}
+function createInterface(tagsFromBase) {
+    console.log(`trying to create text interface in: ${storage.config.tempPath}`);
+
+
+    let interface = `\n================================== tags ======================================\n`;
+    interface +=    ``;
+    interface +=    `\n================================ commands ====================================\n`;
+    interface +=    `save`;
+    interface +=    `\n============================ tags used previously ============================\n`;
+
+    for (var i = tagsFromBase.length; i--;) {
+        interface += tagsFromBase[i];
+        if (i) interface += ', ';
+    }
+
+    console.log(interface);
+
+    mainFLOW.to('finish');
 }
 
 function closeApp(param) {
